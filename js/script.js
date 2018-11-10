9f09db0e507b84e47d46a3ef57618b79e56afb47
Kapanda <katkh13@gmail.com>
2018-11-10 20:25:56 +0200

    Updates

---
+++ js/script.js
@@ -0,0 +1,179 @@
+//Comments slideshow
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("reviews__slides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
+
+//Gallery
+
+function openModal() {
+  document.getElementById('myModal').style.display = "block";
+}
+
+function closeModal() {
+  document.getElementById('myModal').style.display = "none";
+}
+
+var imgIndex = 1;
+showImages(imgIndex);
+
+function plusImages(no) {
+  showImages(imgIndex += no);
+}
+
+function currentImage(no) {
+  showImages(imgIndex = no);
+}
+
+function showImages(no) {
+  var i;
+  var images = document.getElementsByClassName("myImages");
+
+  if (no > images.length) { imgIndex = 1 }
+  if (no < 1) { imgIndex = images.length }
+  for (i = 0; i < images.length; i++) {
+    images[i].style.display = "none";
+  }
+  images[imgIndex - 1].style.display = "block";
+}
+
+
+//Button counter
+
+$('.btn-number').click(function(e){
+  e.preventDefault();
+  
+  fieldName = $(this).attr('data-field');
+  type      = $(this).attr('data-type');
+  var input = $("input[name='"+fieldName+"']");
+  var currentVal = parseInt(input.val());
+  if (!isNaN(currentVal)) {
+      if(type == 'minus') {
+          
+          if(currentVal > input.attr('min')) {
+              input.val(currentVal - 1).change();
+          } 
+          if(parseInt(input.val()) == input.attr('min')) {
+              $(this).attr('disabled', true);
+          }
+
+      } else if(type == 'plus') {
+
+          if(currentVal < input.attr('max')) {
+              input.val(currentVal + 1).change();
+          }
+          if(parseInt(input.val()) == input.attr('max')) {
+              $(this).attr('disabled', true);
+          }
+
+      }
+  } else {
+      input.val(0);
+  }
+});
+
+
+$('.input-number').focusin(function(){
+ $(this).data('oldValue', $(this).val());
+});
+$('.input-number').change(function() {
+  
+  minValue =  parseInt($(this).attr('min'));
+  maxValue =  parseInt($(this).attr('max'));
+  valueCurrent = parseInt($(this).val());
+  
+  name = $(this).attr('name');
+  if(valueCurrent >= minValue) {
+      $(".btn-number[data-type='minus'][data-field='"+name+"']").removeAttr('disabled')
+  } else {
+      alert('Sorry, the minimum value was reached');
+      $(this).val($(this).data('oldValue'));
+  }
+  if(valueCurrent <= maxValue) {
+      $(".btn-number[data-type='plus'][data-field='"+name+"']").removeAttr('disabled')
+  } else {
+      alert('Sorry, the maximum value was reached');
+      $(this).val($(this).data('oldValue'));
+  }
+});
+
+$(".input-number").keydown(function (e) {
+      // Allow: backspace, delete, tab, escape, enter and .
+      if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 190]) !== -1 ||
+           // Allow: Ctrl+A
+          (e.keyCode == 65 && e.ctrlKey === true) || 
+           // Allow: home, end, left, right
+          (e.keyCode >= 35 && e.keyCode <= 39)) {
+               // let it happen, don't do anything
+               return;
+      }
+      // Ensure that it is a number and stop the keypress
+      if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+          e.preventDefault();
+      }
+});
+
+//Menu
+
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const navItems = document.querySelectorAll('.nav-item');
+
+// Set Initial State Of Menu
+let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    menuBtn.classList.add('close');
+    menu.classList.add('show');
+    menuNav.classList.add('show');
+    navItems.forEach(item => item.classList.add('show'));
+
+    // Set Menu State
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove('close');
+    menu.classList.remove('show');
+    menuNav.classList.remove('show');
+    navItems.forEach(item => item.classList.remove('show'));
+
+    // Set Menu State
+    showMenu = false;
+  }
+}
+
+
+jQuery(function($){
+$(".form__user-number").mask("+38 (099) 999 99 99");
+});
+
+
